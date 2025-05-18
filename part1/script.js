e26b0f30bd53a239f981b6c45bd7db59b07d6bc8
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-19 09:30:24 +1200

    test1

---
+++ part1/script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = document.getElementById('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+//Objects in JavaScript our so called reference to data types 
