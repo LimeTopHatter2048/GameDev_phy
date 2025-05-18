e5e4183e8bdfe2cb333dbde5a7ce3ff064e9f467
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-19 09:43:59 +1200

    Player guess?

--- part1/script.js
+++ part1/script.js
@@ -3,5 +3,66 @@ window.addEventListener('load', function(){
     const ctx = document.getElementById('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    class Player {
+        constructor(game){
+            this.game = game;
+            this.image = document.getElementById("player");
+            this.spriteWidth = 100;
+            this.spriteHeight = 91.3;
+            this.width = this.spriteWidth/1.2;
+            this.height = this.spriteHeight/1.2;
+            this.x = this.game.width/2 - this.width/2;
+            this.y = this.game.height - this.height - this.game.groundMargin;
+            this.vy = 0;
+            this.weight = 1;
+            this.frameX = 0;
+            this.frameY = 0;
+            this.maxFrame= 6;
+            this.fps = 20;
+            this.frameTimer = 0;
+            this.frameInterval = 1000/this.fps;
+            this.speed = 0;
+            this.maxSpeed = 10;
+        }
+        update(input, deltaTime){
+            // horizontal movement
+
+            // horizontal boundaries
+
+            // vertical movement
+
+            // vertical boundaries
+            
+            // sprite animation
+            if (this.frameTimer > this.frameInterval){
+                this.frameTimer = 0;
+                if (this.frameX < this.maxFrame) this.frameX++;
+                else this.frameX = 0;
+            } else {
+                this.frameTimer += deltaTime;
+            }
+        }
+        draw(context){
+            context.strokeStyle = 'white';
+            if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        }
+    }
+    class Game {
+        constructor(canvas){
+            this.canvas = canvas;
+            this.width = this.canvas.width;
+            this.height = this.canvas.height;
+            this.player = new Player(this);
+        }
+    }
+
+    const game = new Game(canvas);
+    console.log(game);
+
+    function animate(){
+        
+    }
 });
 //Objects in JavaScript our so called reference to data types 
