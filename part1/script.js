83eea8b3ba605005fbfa78db542e7aab4eb396ec
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-19 15:19:52 +1200

    player movement boundaries

--- part1/script.js
+++ part1/script.js
@@ -75,6 +75,18 @@ window.addEventListener('load', function(){
 
             this.spriteX = this.collisionX - this.width * 0.5;
             this.spriteY = this.collisionY - this.height * 0.5;
+            // horizontal movement
+
+            // horizontal boundaries
+            if(this.collisionX < this.collisionRadius) this.collisionX = this.collisionRadius; 
+            else if (this.collisionX > this.game.width - this.collisionRadius) this.collisionX > this.game.width - this.collisionRadius;
+
+            // vertical movement
+
+            // vertical boundaries
+            if (this.collisionY < this.game.topMargin + this.collisionRadius) this.collisionY = this.game.topMargin + this.collisionRadius;
+            else if (this.collisionY > this.game.height - this.collisionRadius) this.collisionY = this.game.height - this.collisionRadius;
+
             // collisions with obstacles
             this.game.obstacles.forEach(obstacle => {
                 // [(distance < sumOfRadii), distance, sumOfRadii, dx, dy]
@@ -87,13 +99,6 @@ window.addEventListener('load', function(){
                     this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unit_y;
                 }
             })
-            // horizontal movement
-
-            // horizontal boundaries
-
-            // vertical movement
-
-            // vertical boundaries
 
             // sprite animation
             /* if (this.frameTimer > this.frameInterval){
@@ -200,7 +205,7 @@ window.addEventListener('load', function(){
                         overlap = true;
                     }
                 });
-                const margin = testObstacle.collisionRadius * 2;
+                const margin = testObstacle.collisionRadius * 3;
                 if (!overlap && testObstacle.spriteX > 0 && testObstacle.spriteX < this.width - testObstacle.width && testObstacle.collisionY > this.topMargin + margin && testObstacle.collisionY < this.height - margin){
                     this.obstacles.push(testObstacle);
                 }
